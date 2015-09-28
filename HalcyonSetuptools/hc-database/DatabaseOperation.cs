95be174a905ad6274f0af987f669273fe03596c5
David Daeschler <david.daeschler@gmail.com>
2015-09-28 11:38:15 -0500

    Get the initial option reads working

--- HalcyonSetuptools/hc-database/DatabaseOperation.cs
+++ HalcyonSetuptools/hc-database/DatabaseOperation.cs
@@ -1,13 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace hc_database
+﻿namespace hc_database
 {
+    /// <summary>
+    /// The type of operation to be performed
+    /// </summary>
     public enum DatabaseOperation
     {
-        Init
+        /// <summary>
+        /// No operation. The user didn't select an option
+        /// </summary>
+        None,
+
+        /// <summary>
+        /// Database initialization
+        /// </summary>
+        Init,
+
+        /// <summary>
+        /// Database upgrade
+        /// </summary>
+        Upgrade,
+
     }
 }
--- HalcyonSetuptools/hc-database/Program.cs
+++ HalcyonSetuptools/hc-database/Program.cs
@@ -1,34 +1,110 @@
 ﻿using System;
 using NDesk.Options;
 using System.Collections.Generic;
+using MySql.Data.MySqlClient;
 
 namespace hc_database
 {
     class Program
     {
+        static private bool help = false;
         static private DatabaseOperation op;
+
         static private string dbType;
+
+        static private string dbHost;
+        static private string dbUser;
+        static private string dbPass;
+
+        static private OptionSet options = new OptionSet()
+        {
+            { "init",       "Initializes a new halcyon database",               v => op = DatabaseOperation.Init },
+            { "upgrade",    "Upgrades halcyon database",                        v => op = DatabaseOperation.Upgrade },
+            { "t|type=",     "Specifies the halcyon database type (core, rdb)",  v => dbType = v },
+            { "h|host=",     "Specifies the database hostname",                  v => dbHost = v },
+            { "u|user=",     "Specifies the database username",                  v => dbUser = v },
+            { "p|password=", "Specifies the database password",                  v => dbPass = v },
+            { "?|help",   "Prints this help message",                           v => help = v != null },
+        };
 
         static void Main(string[] args)
         {
-            bool help = false;
+            List<string> extra = options.Parse(args);
 
-            var p = new OptionSet() {
-                { "init",       "Initializes a new halcyon database",               v => op = DatabaseOperation.Init },
-                { "t|type",     "Specifies the halcyon database type (core, rdb)",  v => dbType = v },
-                { "h|?|help",   "Prints this help message",                         v => help = v != null },
-            };
-
-            List<string> extra = p.Parse(args);
-
-            if (help)
+            if (help || op == DatabaseOperation.None)
             {
-                Console.WriteLine("Options:");
-                p.WriteOptionDescriptions(Console.Out);
+                PrintUsage();
                 return;
             }
 
+            switch (op)
+            {
+                case DatabaseOperation.Init:
+                    DoInit();
+                    break;
+            }
         }
-        
+
+        private static void PrintUsage()
+        {
+            Console.WriteLine("Options:");
+            options.WriteOptionDescriptions(Console.Out);
+        }
+
+        private static void DoInit()
+        {
+            if (String.IsNullOrEmpty(dbType))
+            {
+                PrintUsage();
+                return;
+            }
+
+            switch (dbType.ToLower())
+            {
+                case "core":
+                    DoInitCore();
+                    break;
+            }
+        }
+
+        private static void DoInitCore()
+        {
+            if (! VerifyDatabaseParameters())
+            {
+                PrintUsage();
+                return;
+            }
+
+            MySqlConnection conn = new MySqlConnection(
+                String.Format("Data Source={0};User ID={1};password={2}",
+                dbHost, dbUser, dbPass));
+
+            try
+            {
+                conn.Open();
+            }
+            catch (MySqlException e)
+            {
+                Console.Error.WriteLine("Unable to connect to the database: {0}", e.Message);
+                return;
+            }
+            
+
+        }
+
+        /// <summary>
+        /// Make sure user filled out host, user, password
+        /// </summary>
+        /// <returns>True if parameters are filled, false if not</returns>
+        private static bool VerifyDatabaseParameters()
+        {
+            if (String.IsNullOrEmpty(dbHost) || String.IsNullOrEmpty(dbUser) 
+                || String.IsNullOrEmpty(dbPass))
+            {
+                return false;
+            }
+
+            return true;
+        }
     }
 }
