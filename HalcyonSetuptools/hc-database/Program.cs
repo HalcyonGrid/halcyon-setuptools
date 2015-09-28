d387aed6bab7b8c85f065d9c647560c989c0005a
David Daeschler <david.daeschler@gmail.com>
2015-09-27 23:42:39 -0500

    Initial commit

---
+++ HalcyonSetuptools/hc-database/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using NDesk.Options;
+using System.Collections.Generic;
+
+namespace hc_database
+{
+    class Program
+    {
+        static private DatabaseOperation op;
+        static private string dbType;
+
+        static void Main(string[] args)
+        {
+            bool help = false;
+
+            var p = new OptionSet() {
+                { "init",       "Initializes a new halcyon database",               v => op = DatabaseOperation.Init },
+                { "t|type",     "Specifies the halcyon database type (core, rdb)",  v => dbType = v },
+                { "h|?|help",   "Prints this help message",                         v => help = v != null },
+            };
+
+            List<string> extra = p.Parse(args);
+
+            if (help)
+            {
+                Console.WriteLine("Options:");
+                p.WriteOptionDescriptions(Console.Out);
+                return;
+            }
+
+        }
+        
+    }
+}
