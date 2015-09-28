ffed39539438517a24f534d8bb2e8b0d8291c953
David Daeschler <david.daeschler@gmail.com>
2015-09-28 13:14:01 -0500

    Finish up work on initial schema creation

--- HalcyonSetuptools/hc-database/Program.cs
+++ HalcyonSetuptools/hc-database/Program.cs
@@ -15,34 +15,47 @@ namespace hc_database
         static private string dbHost;
         static private string dbUser;
         static private string dbPass;
+        static private string dbSchema;
+
+        private const string CORE_DEFAULT_SCHEMA_NAME = "inworldz";
+        private const string RDB_DEFAULT_SCHEMA_NAME = "inworldz_rdb";
+
+        private const string CORE_SCHEMA_BASE_FILE = "inworldz-core-base.sql";
+        private const string RDB_SCHEMA_BASE_FILE = "inworldz-rdb-base.sql";
+
 
         static private OptionSet options = new OptionSet()
         {
-            { "init",       "Initializes a new halcyon database",               v => op = DatabaseOperation.Init },
-            { "upgrade",    "Upgrades halcyon database",                        v => op = DatabaseOperation.Upgrade },
-            { "t|type=",     "Specifies the halcyon database type (core, rdb)",  v => dbType = v },
-            { "h|host=",     "Specifies the database hostname",                  v => dbHost = v },
-            { "u|user=",     "Specifies the database username",                  v => dbUser = v },
-            { "p|password=", "Specifies the database password",                  v => dbPass = v },
-            { "?|help",   "Prints this help message",                           v => help = v != null },
+            { "init",           "Initializes a new halcyon database",               v => op = DatabaseOperation.Init },
+            { "upgrade",        "Upgrades halcyon database",                        v => op = DatabaseOperation.Upgrade },
+            { "t|type=",        "Specifies the halcyon database type (core, rdb)",  v => dbType = v.ToLower() },
+            { "h|host=",        "Specifies the database hostname",                  v => dbHost = v },
+            { "u|user=",        "Specifies the database username",                  v => dbUser = v },
+            { "p|password=",    "Specifies the database password",                  v => dbPass = v },
+            { "s|schema=",      "Specifies the name of the database schema",        v => dbSchema = v },
+            { "?|help",         "Prints this help message",                         v => help = v != null },
         };
 
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
             List<string> extra = options.Parse(args);
 
             if (help || op == DatabaseOperation.None)
             {
                 PrintUsage();
-                return;
+                return 1;
             }
+
+            //allow blank passwords
+            if (dbPass == null) dbPass = String.Empty;
 
             switch (op)
             {
                 case DatabaseOperation.Init:
-                    DoInit();
-                    break;
+                    return DoInit();
             }
+
+            return 1;
         }
 
         private static void PrintUsage()
@@ -51,28 +64,35 @@ namespace hc_database
             options.WriteOptionDescriptions(Console.Out);
         }
 
-        private static void DoInit()
+        private static int DoInit()
         {
             if (String.IsNullOrEmpty(dbType))
             {
                 PrintUsage();
-                return;
+                return 1;
             }
 
-            switch (dbType.ToLower())
+            switch (dbType)
             {
                 case "core":
-                    DoInitCore();
-                    break;
+                    return DoInitCore();
             }
+
+            Console.Error.WriteLine("Unknown database type: " + dbType);
+            return 1;
         }
 
-        private static void DoInitCore()
+        private static int DoInitCore()
         {
+            if (String.IsNullOrEmpty(dbSchema))
+            {
+                dbSchema = CORE_DEFAULT_SCHEMA_NAME;
+            }
+
             if (! VerifyDatabaseParameters())
             {
                 PrintUsage();
-                return;
+                return 1;
             }
 
             MySqlConnection conn = new MySqlConnection(
@@ -86,10 +106,48 @@ namespace hc_database
             catch (MySqlException e)
             {
                 Console.Error.WriteLine("Unable to connect to the database: {0}", e.Message);
-                return;
+                return 1;
             }
-            
 
+            //create the schema if it is missing
+            try
+            {
+                MySqlCommand cmd = conn.CreateCommand();
+                cmd.CommandText = String.Format("CREATE DATABASE IF NOT EXISTS {0};", dbSchema);
+                cmd.ExecuteNonQuery();
+            }
+            catch (MySqlException e)
+            {
+                Console.Error.WriteLine("Unable to create database schema: {0}", e.Message);
+                return 1;
+            }
+
+            //for the core database, we'll actually open up and run both the 
+            //core file and the RDB file. that way, a user can run both from
+            //the same database
+            try
+            {
+                SqlFileRunner coreRunner = new SqlFileRunner(conn, dbSchema, CORE_SCHEMA_BASE_FILE);
+                coreRunner.Run();
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine("Unable to load CORE schema: {0}", e.Message);
+                return 1;
+            }
+
+            try
+            {
+                SqlFileRunner rdbRunner = new SqlFileRunner(conn, dbSchema, RDB_DEFAULT_SCHEMA_NAME);
+                rdbRunner.Run();
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine("Unable to load RDB schema: {0}", e.Message);
+                return 1;
+            }
+
+            return 0;
         }
 
         /// <summary>
@@ -99,7 +157,7 @@ namespace hc_database
         private static bool VerifyDatabaseParameters()
         {
             if (String.IsNullOrEmpty(dbHost) || String.IsNullOrEmpty(dbUser) 
-                || String.IsNullOrEmpty(dbPass))
+                || String.IsNullOrEmpty(dbSchema))
             {
                 return false;
             }
---
+++ HalcyonSetuptools/hc-database/SqlFileRunner.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.IO;
+
+namespace hc_database
+{
+    /// <summary>
+    /// Class that runs an SQL structure dump one statement at a time
+    /// </summary>
+    public class SqlFileRunner
+    {
+        private string[] _commands;
+        private MySqlConnection _conn;
+
+        /// <summary>
+        /// Constructs a new runner that will execute the given file
+        /// </summary>
+        /// <param name="conn">The database connection to execute the commands against</param>
+        /// <param name="filePath">The path of the SQL dump that contains the commands to run</param>
+        public SqlFileRunner(MySqlConnection conn, string schema, string filePath)
+        {
+            _conn = conn;
+            _conn.ChangeDatabase(schema);
+
+            //try to open the file
+            string[] lines = File.ReadAllLines(filePath);
+            List<string> filteredLines = new List<string>(lines.Length);
+
+            //filter commented lines
+            foreach (var line in lines)
+            {
+                if (! line.StartsWith("--"))
+                {
+                    filteredLines.Add(line);
+                }
+            }
+
+            //join and then split by semicolon which should be one statement
+            string joined = String.Join("", filteredLines);
+            _commands = joined.Split(new char[] { ';' });
+        }
+
+        /// <summary>
+        /// Runs the loaded file
+        /// </summary>
+        public void Run()
+        {
+            foreach (var command in _commands)
+            {
+                var cmd = _conn.CreateCommand();
+                cmd.CommandText = command;
+
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                catch (MySqlException e)
+                {
+                    throw new Exception(String.Format("Error {0}\nWhile executing query {1}", 
+                        e.Message, command), e);
+                }
+                
+            }
+        }
+    }
+}
